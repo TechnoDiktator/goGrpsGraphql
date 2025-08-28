5ba6784789fbc0ea40efdea77e20dbe27ba88f96
Rastogi <tarang.rastogi@hp.com>
2025-08-28 18:01:17 +0530

    new code autosave problem fixed

--- graphql/models.go
+++ graphql/models.go
@@ -1,1 +1,19 @@
 package main
+
+type Account struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+	// Orders []*Order `json:"orders"`  // commented out because we want autogen to generate a resolver for this
+}
+
+/*
+
+
+type Account{
+    id: String!
+    name: String!
+    orders: [Order!]!
+}
+
+
+*/
